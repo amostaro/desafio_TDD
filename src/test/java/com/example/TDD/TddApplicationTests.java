338634d048f010d23caa98aa495e979a5170515b
amostaro <amostaro@ciandt.com>
2022-07-22 10:06:41 -0300

    initial commit tests

--- src/test/java/com/example/TDD/TddApplicationTests.java
+++ src/test/java/com/example/TDD/TddApplicationTests.java
@@ -6,12 +6,53 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TddApplicationTests<Ingrediente> {
 
+//	@Test
+//	void contextLoads() {
+//	}
+
 	@Test
-	void contextLoads() {
+	public void testCadastrarIngredienteEmEstoque(Ingrediente ingrediente) {
+		try {
+
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("Ingrediente já cadastrado");
+		}
 	}
 
-	public void cadastrarIngredienteEmEstoque(Ingrediente ingrediente) {
+	@Test
+	public void testDescadastrarIngredienteEmEstoque(Ingrediente ingrediente) {
+		try {
 
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("Ingrediente não encontrado");
+		}
+	}
+
+	@Test
+	public void testAdicionarQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente, Integer quantidade) {
+		try {
+
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("Ingrediente não encontrado ou quantidade inválida");
+		}
+	}
+
+	@Test
+	public void testReduzirQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente, Integer quantidade) {
+		try {
+
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("Ingrediente não encontrado ou quantidade inválida");
+		}
+	}
+
+	@Test
+	public void testConsultarQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente) {
+		try {
+
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("Ingrediente não encontrado");
+		}
 	}
 
 }
